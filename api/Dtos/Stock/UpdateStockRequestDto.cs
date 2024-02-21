e792acca0a89983895d4de2b434145db57891493
Joao Martins <milanezijoao51@gmail.com>
2024-02-21 11:22:07 -0300

    Add Data Validation

--- api/Dtos/Stock/UpdateStockRequestDto.cs
+++ api/Dtos/Stock/UpdateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,23 @@ namespace api.Dtos.Stock
 {
     public class UpdateStockRequestDto
     {
+        [Required]
+        [MaxLength(10,ErrorMessage ="Symbol cannot be over 10 charactes")]        
         public string Symbol { get; set; }=string.Empty;
+        [Required]
+        [MaxLength(15,ErrorMessage ="Copany name cannot be over 15 charactes")]       
         public string CompanyName { get; set; }=string.Empty;
+        [Required]
+        [Range(1,1000000000000)]
         public decimal Purchase { get; set; }
+        [Required]
+        [Range(0.001,100)]
         public decimal LastDiv { get; set; }
+        [Required]
+        [MaxLength(15,ErrorMessage ="Industry cannot be over 15 charactes")]       
         public string Industry { get; set; }=string.Empty;
+      
+        [Range(1,5000000000000)]
         public long MarketCap { get; set; }
     }
 }
